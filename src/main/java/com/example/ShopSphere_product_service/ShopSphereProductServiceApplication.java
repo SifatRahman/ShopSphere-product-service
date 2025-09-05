f74b5dacd8f3aa244a4a8a0efa0fff7428bf9fbf
Sifat <sifatur.rahman.sifat@dohatec.com.bd>
2025-09-05 13:53:04 +0600

    first user commit

---
+++ src/main/java/com/example/ShopSphere_product_service/ShopSphereProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopSphere_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSphereProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSphereProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShopSphere_product_service/ShopSphereProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopSphere_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSphereProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
